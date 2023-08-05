d1d8b5d1eda4831379a3f3e96b958e4823e66bac
bharadwaj23 <76489273+bharadwaj23@users.noreply.github.com>
2023-08-05 16:15:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,46 @@ let tasksContainer = document.getElementById('tasksContainer');
 let userInput = document.getElementById('userInput');
 let addButton = document.getElementById('addButton');
 let saveButton = document.getElementById('save');
+let loadIcon = document.getElementById('loadIcon');
+let swipeThreshold = 100;
 
+
+loadIcon.classList.add('d-none');
 userInput.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
         // Trigger the click event of the submit button
         addButton.click();
     }
 });
+
+
+function findCurrentdateTime() {
+    let now = new Date();
+    let minutes = null;
+    let month = null;
+    let hour = null;
+
+    if (now.getMinutes() <= 9) {
+        minutes = "0" + now.getMinutes();
+    } else {
+        minutes = now.getMinutes();
+    }
+
+    if ((now.getMonth() + 1) <= 9) {
+        month = "0" + (now.getMonth() + 1);
+    } else {
+        month = now.getMonth() + 1;
+    }
+
+    if (now.getHours() <= 9) {
+        hour = "0" + now.getHours();
+    } else {
+        hour = now.getHours();
+    }
+
+
+    return `${now.getDate()}-${month}-${now.getFullYear()} ${hour}:${minutes}`;
+}
 
 function getTodoListFromLocalStorage() {
     let storedValue = localStorage.getItem("TodoList");
@@ -24,13 +57,20 @@ let todoList = getTodoListFromLocalStorage();
 
 saveButton.onclick = function() {
     localStorage.setItem('TodoList', JSON.stringify(todoList));
+    loadIcon.classList.remove('d-none');
+    saveButton.classList.add('d-none');
+    setTimeout(function() {
+        loadIcon.classList.add('d-none');
+        saveButton.classList.remove('d-none');
+    }, 1000);
 }
 
-let todoListLength = todoList.length;
-
 function onDeleteTodo(taskItem) {
-    let taskContainer = document.getElementById(taskItem);
-    tasksContainer.removeChild(taskContainer);
+    let listContainer = document.getElementById(taskItem);
+    listContainer.style.transform = "translateX(150%)";
+    setTimeout(function() {
+        tasksContainer.removeChild(listContainer);
+    }, 300);
 
     let indexOfTodo = todoList.findIndex(function(eachItem) {
         eachItem = "todo" + eachItem.uniqueNo;
@@ -41,43 +81,75 @@ function onDeleteTodo(taskItem) {
     todoList.splice(indexOfTodo, 1); //remove no of elements from the given index 
 }
 
+function onTouchStart(event) {
+    this.startX = event.touches[0].clientX;
+    this.startY = event.touches[0].clientY;
+}
+
+function onTouchEnd(event) {
+    let endX = event.changedTouches[0].clientX;
+    let endY = event.changedTouches[0].clientY;
+    let deltaX = endX - this.startX;
+    let deltaY = endY - this.startY;
+
+    if (Math.abs(deltaX) > Math.abs(deltaY) && Math.abs(deltaX) > swipeThreshold) {
+        if (deltaX > 0) {
+            // Right-to-left swipe
+            let taskItem = this.getAttribute('id');
+            onDeleteTodo(taskItem);
+        } else {
+            // Left-to-right swipe
+            checkbox.click(); // Trigger the checkbox click event
+        }
+    }
+}
+
 function createTodoItem(todoItem) {
     let taskItem = 'todo' + todoItem.uniqueNo;
     let uniqueId = 'myTask' + todoItem.uniqueNo;
+    let time = todoItem.time;
 
     let listContainer = document.createElement('li');
     listContainer.id = taskItem;
-    listContainer.classList.add('mt-3', 'mb-3', 'list-container', 'd-flex', 'flex-row');
+    listContainer.classList.add('task-container');
+    listContainer.addEventListener('touchstart', onTouchStart);
+    listContainer.addEventListener('touchend', onTouchEnd);
     tasksContainer.prepend(listContainer); //prepend is oppposite of append
 
     let checkbox = document.createElement('input');
     checkbox.id = uniqueId;
     checkbox.setAttribute('type', 'checkbox');
-    checkbox.classList.add('m-2', 'checkbox');
+    checkbox.classList.add('checkbox');
     checkbox.checked = todoItem.isChecked;
     listContainer.appendChild(checkbox);
 
     let labelContainer = document.createElement('div');
-    labelContainer.classList.add('w-100', 'd-flex', 'flex-row', 'label-container');
+    labelContainer.classList.add('label-container');
     listContainer.appendChild(labelContainer);
+
+    let workContainer = document.createElement('div');
+    workContainer.classList.add('work-container');
+    labelContainer.appendChild(workContainer);
 
     let task = document.createElement('label');
     task.setAttribute('for', uniqueId);
-    task.classList.add('w-100', 'pt-1', 'task');
+    task.classList.add('task');
     task.textContent = todoItem.text;
-    labelContainer.appendChild(task);
+    workContainer.appendChild(task);
 
-    let deleteContainer = document.createElement('div');
-    deleteContainer.classList.add('d-flex', 'flex-column', 'justify-content-center');
-    labelContainer.appendChild(deleteContainer);
+    let todoTime = document.createElement('p');
+    todoTime.classList.add('time');
+    todoTime.textContent = time;
+    labelContainer.appendChild(todoTime);
 
     let deleteIcon = document.createElement('i');
-    deleteIcon.classList.add('m-2', 'fa-solid', 'fa-trash-can');
+    deleteIcon.classList.add('delete-icon', 'fa-solid', 'fa-trash-can');
     deleteIcon.onclick = function() {
         onDeleteTodo(taskItem);
 
     };
-    deleteContainer.appendChild(deleteIcon);
+    listContainer.appendChild(deleteIcon);
+
     if (checkbox.checked) {
         task.classList.add('checked');
         deleteIcon.classList.add('fa-bounce');
@@ -94,14 +166,15 @@ function createTodoItem(todoItem) {
             task.classList.remove('checked');
         }
     };
+
 }
 
 function addNewTodo(newTask, uniqueNumber) {
-    todoListLength = todoListLength + 1;
     let newTodo = {
         text: newTask,
-        uniqueNo: todoListLength,
-        isChecked: false
+        uniqueNo: uniqueNumber,
+        isChecked: false,
+        time: findCurrentdateTime()
     };
     todoList.push(newTodo); // opposite is unshift
     createTodoItem(newTodo);
@@ -109,7 +182,7 @@ function addNewTodo(newTask, uniqueNumber) {
 
 addButton.onclick = function() {
     let newTask = userInput.value;
-    let uniqueNumber = todoList.length;
+    let uniqueNumber = todoList.length + 1;
     if (newTask !== "") {
         addNewTodo(newTask, uniqueNumber);
         userInput.value = "";
@@ -119,7 +192,7 @@ addButton.onclick = function() {
 }
 
 if (todoList.length !== 0) {
-    for (let todoitem of todoList) {
-        createTodoItem(todoitem);
+    for (let todoItem of todoList) {
+        createTodoItem(todoItem);
     }
 }
